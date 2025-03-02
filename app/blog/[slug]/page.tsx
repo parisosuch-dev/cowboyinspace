de8cb9d16042e949a85ecae04d891a1ddef549b5
paris osuch <paris.osuch@gmail.com>
2025-03-01 16:48:45 -0800

    removing padding

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -34,7 +34,7 @@ const postPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
   // TODO: styling for this shit
   return (
     <div className="w-full flex flex-col items-center">
-      <div className="p-4 sm:p-8 space-y-4 sm:space-y-8 sm:w-1/3 font-mono">
+      <div className="space-y-4 sm:space-y-8 sm:w-1/3 font-mono">
         <Link
           className="text-xs sm:text-sm hover:underline hover:cursor-pointer"
           href="/"

3907e97a462c7b52b611517963628a8ffad4e615
paris osuch <paris.osuch@gmail.com>
2025-03-01 16:53:44 -0800

    fix width

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -34,7 +34,7 @@ const postPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
   // TODO: styling for this shit
   return (
     <div className="w-full flex flex-col items-center">
-      <div className="space-y-2 sm:space-y-4 sm:w-1/3 font-mono">
+      <div className="space-y-2 sm:space-y-4 w-5/6 sm:w-1/3 font-mono">
         <Link
           className="text-xs sm:text-sm hover:underline hover:cursor-pointer"
           href="/"

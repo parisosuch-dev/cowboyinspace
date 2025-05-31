b9f295ea45391abe4999bf7cd0f1ddbe422d6862
paris osuch <paris.osuch@gmail.com>
2025-05-30 22:24:35 -0700

    progress

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { ShootingStars } from "./ui/shooting-stars";
 import { StarsBackground } from "./ui/stars-background";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 
 export default function Header({
   title,
@@ -8,16 +12,41 @@ export default function Header({
   title: string;
   subtext?: string;
 }) {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  if (theme === "light") {
+    return (
+      <div className="h-[7rem] sm:h-[10rem]  rounded-md dark:bg-white bg-neutral-900 flex flex-col text-left p-4 justify-center relative font-mono">
+        <h1 className="relative z-10 text-2xl md:text-5xl dark:text-neutral-900 text-white font-semibold">
+          {title}
+        </h1>
+        {subtext ? (
+          <p className="dark:text-neutral-900 text-white text-xs sm:text-sm mt-1 sm:mt-2">
+            {subtext}
+          </p>
+        ) : null}
+        <ShootingStars />
+        <StarsBackground />
+      </div>
+    );
+  }
   return (
-    <div className="h-[7rem] sm:h-[10rem]  rounded-md bg-neutral-900 flex flex-col text-left p-4 justify-center relative font-mono">
-      <h1 className="relative z-10 text-2xl md:text-5xl text-white font-semibold">
+    <div className="h-[7rem] sm:h-[10rem]  rounded-md dark:bg-white bg-neutral-900 flex flex-col text-left p-4 justify-center relative font-mono">
+      <h1 className="relative z-10 text-2xl md:text-5xl dark:text-neutral-900 text-white font-semibold">
         {title}
       </h1>
       {subtext ? (
-        <p className="text-white text-xs sm:text-sm mt-1 sm:mt-2">{subtext}</p>
+        <p className="dark:text-neutral-900 text-white text-xs sm:text-sm mt-1 sm:mt-2">
+          {subtext}
+        </p>
       ) : null}
-      <ShootingStars />
-      <StarsBackground />
     </div>
   );
 }

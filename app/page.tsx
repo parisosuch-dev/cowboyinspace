83256e31567532ff044637ceb7aa350c034a7414
paris osuch <paris.osuch@gmail.com>
2025-05-30 20:20:32 -0700

    add other site section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,17 @@ import Link from "next/link";
 export default function Home() {
   const post = getLatestPosts(1)[0];
 
+  const sites = [
+    {
+      title: "My resume",
+      url: "parisosuch.com",
+    },
+    {
+      title: "I really like pizza",
+      url: "parisloves.pizza",
+    },
+  ];
+
   return (
     <div className="flex w-full flex-col justify-center items-center">
       <div className="w-5/6 sm:w-3/6 2xl:w-1/3">
@@ -40,6 +51,26 @@ export default function Home() {
               More posts &rarr;
             </Link>
           </div>
+        </div>
+        <div className="mt-2 sm:mt-8 border border-gray-100 rounded-md p-4">
+          <h2 className="font-mono text-xl md:text-4xl font-semibold">
+            Other Sites
+          </h2>
+          {sites.map((site) => (
+            <Link
+              key={site.title}
+              href={"https://" + site.url}
+              target="_blank"
+              className="flex flex-col sm:flex-row w-full justify-between font-mono group mt-2"
+            >
+              <p className="text-base md:text-2xl group-hover:text-black/50 dark:group-hover:text-white/50">
+                {site.title}
+              </p>
+              <p className="text-black/50 dark:text-white/50 text-sm md:text-lg">
+                {site.url}
+              </p>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
